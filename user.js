d61fe537052b0e6ce35bc6fe59356e29df302b22
salamovil <salamovil@academico.icesi.edu.co>
2023-08-30 07:47:28 -0500

    ZOD

---
+++ user.js
@@ -0,0 +1,15 @@
+// instalar un nuevo node llamado ZOD el cual nos ayuda a generar reglas sobre nuestro objeto.
+
+//https://www.npmjs.com/package/zod
+
+
+//construir esquemas(estrutura general)
+
+//exportar el uso disponible
+
+
+
+
+const z = require ('zod')
+
+const userSchema = z.object 
